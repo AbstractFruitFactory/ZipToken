84b3c3e5c717ff8cafaaee3357c309c89c1e3006
alexmedkex <alexmedkex@gmail.com>
2018-02-12 22:24:01 +0100

    Added reading from sample file.

--- test_script.js
+++ test_script.js
@@ -1,6 +1,9 @@
 const Web3 = require('web3');
 const solc = require('solc');
 const fs = require('fs');
+var lineReader = require('line-reader');
+
+const readline = require('readline');
 
 function findImports(path) {
     return {
@@ -8,53 +11,40 @@ function findImports(path) {
     }
 }
 
-web3 = new Web3(new Web3.providers.HttpProvider("https://localhost:9545"));
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 code = fs.readFileSync('./contracts/ZipToken.sol').toString();
-
 var inputs = {
     'ZipToken.sol': fs.readFileSync('./contracts/ZipToken.sol').toString(),
 };
 
 compiledCode = solc.compile({sources: inputs}, 1, findImports);
 abiDefinition = JSON.parse(compiledCode.contracts['ZipToken.sol:ZipToken'].interface);
-contract = new web3.eth.Contract(abiDefinition);
+contract = web3.eth.contract(abiDefinition);
 byteCode = compiledCode.contracts['ZipToken.sol:ZipToken'].bytecode;
-//deployedContract = contract.deploy({data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
 
-contract.deploy({
-    data: byteCode
-})
-.send({
-    from: web3.eth.accounts[0],
-    gas: 1500000,
-    gasPrice: '30000000000000'
-}, function(error, transactionHash){ console.log(error) })
-.on('error', function(error){ console.log(error) })
-.on('transactionHash', function(transactionHash){ console.log(transactionHash) })
-.on('receipt', function(receipt){
-   console.log(receipt.contractAddress) // contains the new contract address
-})
-.on('confirmation', function(confirmationNumber, receipt){ console.log(confirmationNumber) })
-.then(function(newContractInstance){
-    console.log(newContractInstance.options.address) // instance with the new contract address
-});
+deployedContract = contract.new({ data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
 
-var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('sampledata')
-});
-  
+contractInstance = contract.at(deployedContract.address);
+
+
 var addresses = [];
 var values = [];
 
-lineReader.on('line', function (line) {
+
+lineReader.eachLine('sampledata.txt', function(line, last) {
     let address = line.split(',')[0];
     let value = line.split(',')[1];
     addresses.push(address);
     values.push(value);
+    if(last){
+        distribute();
+    }
 });
 
-contract.methods.distributeTokens(addresses, values).send({ from: web3.eth.accounts[0] });
+function distribute() {
+    contractInstance.distributeTokens(addresses, values, { from: web3.eth.accounts[0] });
+    console.log(contractInstance.balanceOf('0xf17f52151ebef6c7334fad080c5704d77216b732'));
+}
 
-console.log(contract.methods.balanceOf('0xf17f52151ebef6c7334fad080c5704d77216b732').call());
 
 
